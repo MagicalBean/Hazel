cbe80880f7a0078ceadc39739324c41985cd3ddb
MagicalBean <ben.weisz13@gmail.com>
2020-02-17 20:02:35 -0800

    Instrumentation and small aditions to 2D Renderer.

--- Hazel/src/Hazel/Core/LayerStack.cpp
+++ Hazel/src/Hazel/Core/LayerStack.cpp
@@ -29,6 +29,7 @@ namespace Hazel {
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), layer);
 		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
+			layer->OnDetach();
 			m_Layers.erase(it);
 			m_LayerInsertIndex--;
 		}
@@ -38,6 +39,7 @@ namespace Hazel {
 	{
 		auto it = std::find(m_Layers.begin(), m_Layers.end(), overlay);
 		if (it != m_Layers.end())
+			overlay->OnDetach();
 			m_Layers.erase(it);
 	}
 }
