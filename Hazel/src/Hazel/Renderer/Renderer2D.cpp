494cff7a3aebe287076d7a13124e71ef0882dd41
MagicalBean <ben.weisz13@gmail.com>
2020-07-24 23:38:50 -0700

    Started adding ENTT.

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -155,12 +155,39 @@ namespace Hazel {
 		s_Data.TextureSlotIndex = 1;
 	}
 
+	// =========================== DRAW QUADS =========================== 
+
 	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
 	{
 		DrawQuad({ position.x, position.y, 0.0f }, size, color);
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
+	{
+		HZ_PROFILE_FUNCTION();
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+		
+		DrawQuad(transform, color);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+	{
+		DrawQuad({ position.x, position.y, 0.0f }, size, texture, tilingFactor, tintColor);
+	}
+
+	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+	{
+		HZ_PROFILE_FUNCTION();
+
+		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
+			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
+
+		DrawQuad(transform, texture, tilingFactor, tintColor);
+	}
+
+	void Renderer2D::DrawQuad(const glm::mat4& transform, const glm::vec4& color)
 	{
 		HZ_PROFILE_FUNCTION();
 
@@ -171,9 +198,6 @@ namespace Hazel {
 
 		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
 			FlushAndReset();
-
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
-			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
 		for (size_t i = 0; i < quadVertexCount; i++) {
 			s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[i];
@@ -187,14 +211,16 @@ namespace Hazel {
 		s_Data.QuadIndexCount += 6;
 
 		s_Data.Stats.QuadCount++;
+
 	}
 
-	void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+
+	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
-		DrawQuad({ position.x, position.y, 0.0f }, size, texture, tilingFactor, tintColor);
+		DrawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, color);
 	}
 
-	void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
+	void Renderer2D::DrawQuad(const glm::mat4& transform, const Ref<Texture2D>& texture, float tilingFactor, const glm::vec4& tintColor)
 	{
 		HZ_PROFILE_FUNCTION();
 
@@ -225,9 +251,6 @@ namespace Hazel {
 			s_Data.TextureSlotIndex++;
 		}
 
-		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
-			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
-
 		for (size_t i = 0; i < quadVertexCount; i++)
 		{
 			s_Data.QuadVertexBufferPtr->Position = transform * s_Data.QuadVertexPositions[i];
@@ -242,12 +265,7 @@ namespace Hazel {
 
 		s_Data.Stats.QuadCount++;
 	}
-
-	void Renderer2D::DrawRotatedQuad(const glm::vec2& position, const glm::vec2& size, float rotation, const glm::vec4& color)
-	{
-		DrawRotatedQuad({ position.x, position.y, 0.0f }, size, rotation, color);
-	}
-
+	
 	void Renderer2D::DrawRotatedQuad(const glm::vec3& position, const glm::vec2& size, float rotation, const glm::vec4& color)
 	{
 		HZ_PROFILE_FUNCTION();
---
+++ Hazel/src/Hazel/Scene/Scene.cpp
@@ -0,0 +1,57 @@
+#include "hzpch.h"
+#include "Scene.h"
+
+#include "Components.h"
+#include "Hazel/Renderer/Renderer2D.h"
+
+#include <glm/glm.hpp>
+
+namespace Hazel {
+
+	Scene::Scene()
+	{
+#if ENTT_EXAMPLE_CODE
+		entt::entity entity = m_Registry.create();
+		m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
+
+		if (m_Registry.has<TransformComponent>(entity))
+			TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+
+		auto view = m_Registry.view<TransformComponent>();
+		for (auto entity : view)
+		{
+			TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
+		}
+
+		auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
+		for (auto entity : group)
+		{
+			auto& [transform, mesh] = m_Registry.get<TransformComponent, MeshComponent>(entity);
+
+			// Renderer::Submit(mesh, transform);
+		}
+#endif
+	}
+
+	Scene::~Scene()
+	{
+
+	}
+
+	entt::entity Scene::CreateEntity()
+	{
+		return m_Registry.create();
+	}
+
+	void Scene::OnUpdate(Timestep ts)
+	{
+		auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
+		for (auto entity : group)
+		{
+			auto& [transform, sprite] = m_Registry.get<TransformComponent, SpriteRendererComponent>(entity);
+
+			Renderer2D::DrawQuad(transform, sprite.Color);
+		}
+	}
+
+}
