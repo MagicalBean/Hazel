f1c19b1e6d542ae66934506c637cad8901c4830e
Ben <ben.weisz13@gmail.com>
2020-08-27 16:21:35 -0700

    Added Scene Hierarchy panel

--- Hazelnut/src/EditorLayer.cpp
+++ Hazelnut/src/EditorLayer.cpp
@@ -70,6 +70,8 @@ namespace Hazel {
 		
 		m_CameraEntity.AddComponent<NativeScriptComponent>().Bind<CameraController>();
 		m_SecondCamera.AddComponent<NativeScriptComponent>().Bind<CameraController>();
+
+		m_SceneHeirarchyPanel.SetContext(m_ActiveScene);
 	}
 	
 	void EditorLayer::OnDetach()
@@ -171,6 +173,8 @@ namespace Hazel {
 
 			ImGui::EndMenuBar();
 		}
+
+		m_SceneHeirarchyPanel.OnImGuiRender();
 
 		ImGui::Begin("Settings");
 
